d1cc0786d7b9ec0addff6eb1775234d92552bb24
Will Murray <willdavidmurray@gmail.com>
2018-05-18 22:52:58 +0200

    Add script and try setting up 2 wordpress instances

---
+++ start.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Get the latest from the repo
+git pull
+
+# Kill everything first
+docker rm -f $(docker ps -aq)
+
+# Start the nginx-proxy container and disconnect from it
+docker-compose -f -d ./proxy/docker-compose.yml up -d
+
+for number in {1..2}
+do
+
+# Dynamically create ENV vars
+echo "I hope you changed the password!"
+export MYSQL_PASSWORD=kgB7yJCwGYq2jeQH
+export WP_ID=wp${number}
+export WP_HOST=wp${number}.wordpress.wdmtech.co.uk
+export MYSQL_SUFFIX=mysql_${number}
+
+# Start a wordpress container and disconnect from it
+docker-compose -f -d ./wordpress/docker-compose.yml up -d
+
+echo "Finished setting up Wordpress blog number ${number}"
+
+done
+
+exit 0
