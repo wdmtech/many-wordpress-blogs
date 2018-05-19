bcfa60c5b4fa14425d6701a5e9b1b1b917e8aa56
Will Murray <willdavidmurray@gmail.com>
2018-05-19 20:57:49 +0200

    Copy docker-compose.yml in loop and run those files instead of existing approach

--- start.sh
+++ start.sh
@@ -12,6 +12,8 @@ docker-compose -f ./proxy/docker-compose.yml up -d
 for number in {1..2}
 do
 
+rm -rf ./wordpress/wp${number}
+
 # Dynamically create ENV vars
 echo "I hope you changed the password!"
 export MYSQL_PASSWORD=kgB7yJCwGYq2jeQH
@@ -19,12 +21,11 @@ export WP_ID=wp${number}
 export WP_HOST=wp${number}.wordpress.wdmtech.co.uk
 export MYSQL_SUFFIX=mysql_${number}
 
-echo "Starting $WP_HOST"
+mkdir ./wordpress/wp${number}
+cp ./wordpress/docker-compose.yml ./wordpress/wp${number}
+docker-compose -f ./wordpress/wp${number}/docker-compose.yml up -d
 
-# Start a wordpress container and disconnect from it
-docker-compose -f ./wordpress/docker-compose.yml up -d
-
-echo "Finished setting up Wordpress blog number ${number}"
+echo "Configured a Wordpress blog for $WP_HOST"
 
 done
 
